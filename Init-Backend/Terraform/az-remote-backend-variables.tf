aa7460821daf2ea00a05ea48198b371f5180b8d5
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2024-05-02 19:58:27 +0100

    Initial commit

---
+++ Init-Backend/Terraform/az-remote-backend-variables.tf
@@ -0,0 +1,18 @@
+# company
+variable "company" {
+  type = string
+  description = "This variable defines the name of the company"
+}
+
+# environment
+variable "environment" {
+  type = string
+  description = "This variable defines the environment to be built"
+}
+
+# azure region
+variable "location" {
+  type = string
+  description = "Azure region where resources will be created"
+  default = "west europe"
+}

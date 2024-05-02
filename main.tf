ba18ecd2081f1a85e631dee3e55826e0ca372af2
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2024-05-02 22:22:14 +0100

    Initial commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+# Create the Resource Group
+resource "azurerm_resource_group" "this" {
+  name     = "kopicloud-github-actions-rg"
+  location = "west europe"
+}
+
+# Create the VNET
+resource "azurerm_virtual_network" "this" {
+  name                = "kopicloud-github-actions-vnet"
+  address_space       = ["10.10.10.0/16"]
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+}
+
+# Create the Subnet
+resource "azurerm_subnet" "this" {
+  name                = "kopicloud-github-actions-subnet"  
+  address_prefix       = "10.10.10.0/24"
+  virtual_network_name = azurerm_virtual_network.this.name
+  resource_group_name  = azurerm_resource_group.this.name
+}

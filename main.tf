8efe281ea1479635023c4876a694a23b61516058
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2024-05-04 10:20:23 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "this" {
 # Create the VNET
 resource "azurerm_virtual_network" "this" {
   name                = "kopicloud-github-actions-vnet"
-  address_space       = ["10.10.10.0/16"]
+  address_space       = ["10.10.0.0/16"]
   resource_group_name = azurerm_resource_group.this.name
   location            = azurerm_resource_group.this.location
 }
@@ -15,7 +15,7 @@ resource "azurerm_virtual_network" "this" {
 # Create the Subnet
 resource "azurerm_subnet" "this" {
   name                 = "kopicloud-github-actions-subnet"  
-  address_prefixes     = ["10.10.10.0/24"]
+  address_prefixes     = ["10.10.1.0/24"]
   virtual_network_name = azurerm_virtual_network.this.name
   resource_group_name  = azurerm_resource_group.this.name
 }
